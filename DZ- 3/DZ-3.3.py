24935c9c8a889ba9d700940af24ed70fa7fa8064
05050 <0505060173a@gmail.com>
2022-07-13 18:10:26 +0300

    Конец

--- DZ- 3/DZ-3.3.py
+++ DZ- 3/DZ-3.3.py
@@ -1,4 +1,16 @@
 x = float(input("Введите число: "))
 y = float(input("Введите второе число: "))
 с = input("Выберите действие(+,-,/,*: ")
+if с == "+":
+    print(x+y)
+elif с == "-":
+    print(x-y)
+elif с == "*":
+    print(x*y)
+elif с == "/":
+    if y != 0:
+        print(x/y)
+    else:
+        print("на ноль не делится")
+
 
