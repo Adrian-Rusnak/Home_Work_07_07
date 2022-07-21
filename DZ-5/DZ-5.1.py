6b29e939b2360e7ff7f839a24bc80951bd0f7be8
05050 <0505060173a@gmail.com>
2022-07-21 21:24:04 +0300

    Исправил

--- DZ-5/DZ-5.1.py
+++ DZ-5/DZ-5.1.py
@@ -1,18 +1,4 @@
-x = float(input("Введите число: "))
-l = float(input("Введите второе число: "))
-с = input("Выберите действие  +,-,/,* : ")
-if с == "+":
-    print(x+l)
-elif с == "-":
-    print(x-l)
-elif с == "*":
-    print(x*l)
-elif с == "/":
-    if l != 0:
-        print(x/l)
-else:
-    print("на ноль не делится")
-b = input("Хотите продолжить? (y/n): ")
+b = "y"
 while b == "y":
     x = float(input("Введите число: "))
     l = float(input("Введите второе число: "))
@@ -28,6 +14,7 @@ while b == "y":
             print(x / l)
         else:
             print("на ноль не делится")
-    b = input("Хотите продолжить? (y/n): ")
+    b = input("Хотите продолжить? (y/n): ").lower()
+
 if b == "n":
     print("Робота окончена")
