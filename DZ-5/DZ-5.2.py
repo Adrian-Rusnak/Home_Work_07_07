15f67c8ba5c379ad5460ce8d1f9cd5ebd24bcfdb
05050 <0505060173a@gmail.com>
2022-07-24 20:36:55 +0300

    выполнение

--- DZ-5/DZ-5.2.py
+++ DZ-5/DZ-5.2.py
@@ -1,20 +1,18 @@
 import keyword
 a = keyword.kwlist
-c = [ '''*!"#$%&'()*+,-./:;<=>?@[\]^`{|}~''']
 b = input("Введите переменную: ")
-for i in a:
+for i in a :
     if i == b:
         print(False)
+if [s for s in b[0] if s in '''!"#$%&'()*+,-./:;<=>?@[\]^`{|}~''']:
+    print(False)
 if b.isupper():
     print(False)
 if b.istitle():
     print(False)
-for i in c:
-    if i[0]==b:
-       print(False)
 if b.isdigit():
     print(False)
 if [s for s in b[0] if s in '1234567890']:
     print(False)
-else:
-    print(True)
+
+
