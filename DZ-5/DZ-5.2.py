a2215281d1254ebbc07f8ab505377e3c5b63e43f
05050 <0505060173a@gmail.com>
2022-07-24 14:30:03 +0300

    выполнение

--- DZ-5/DZ-5.2.py
+++ DZ-5/DZ-5.2.py
@@ -1,8 +1,12 @@
 import keyword
-a = a = keyword.kwlist
-
-b = input("Введите переменную:  ")
-for i in a :
-    if i == b:
+a = keyword.kwlist
+c = {*'''!"#$%&'()*+,-./:;<=>?@[\]^`{|}~'''}
+b = input("Введите переменную: ")
+for i in a:
+    if i == b :
         print(False)
-
+for i in b:
+    if i[0].isupper():
+        print(False)
+    elif i[0] in b == c :
+        print( false)
