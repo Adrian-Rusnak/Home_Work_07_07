2224bbc0d15381aa124ef6038caae4cc39e9b737
05050 <0505060173a@gmail.com>
2022-07-31 21:05:59 +0300

    Выполнение

--- DZ-6/DZ-13.6.2.py
+++ DZ-6/DZ-13.6.2.py
@@ -1,12 +1,7 @@
 b = input("Введите число: ")
 c = str.split(b)
 c = ''.join(c)
-if len(c)== 1:
-    c = 1
-elif len(c)== 2:
-    c = str(int(c[0])*int(c[1]))
-elif len(c)==3:
-    c = str(int(c[0]) * int(c[1])*int(c[2]))
+
 while int(c) >9:
     if len(c) == 1:
         c = 1
