71ddb218c8ad16d83dfc627fa5e2479bd00dd0aa
05050 <0505060173a@gmail.com>
2022-08-03 22:17:24 +0300

    Выполненo

--- DZ-7/DZ- 14.7.2.py
+++ DZ-7/DZ- 14.7.2.py
@@ -1,17 +1,12 @@
 def propozition(A, B):
     A = A.title()
-
     B = ''.join(B)
     if B[-1] ==".":
         return A, B
-
     else:
         B = B.split()
         B.append(".")
-
     B = ''.join(B)
     return A,B
-
-
-d = propozition('greetings ', 'friends.')
+d = propozition('Greetings ', 'friends.')
 print(d)
