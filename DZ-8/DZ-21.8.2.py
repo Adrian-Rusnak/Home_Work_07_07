f8dd2f7f323403cd1ea7992140ce4122d337dafe
05050 <0505060173a@gmail.com>
2022-08-09 20:50:49 +0300

    Выполненo

--- DZ-8/DZ-21.8.2.py
+++ DZ-8/DZ-21.8.2.py
@@ -1,16 +1,16 @@
 import string
 n = string.punctuation
-a = input("Введите предложени: ")
-def is_poliandrome(a):
-
+a = input("Введите предложени: ").lower()
+def is_polindrome(a):
+    for i in a:
+        if i in n:
+            a = a.replace(i,'')
     b = a[::-1]
     while True:
         if a[::1] ==b:
-
             return True
             break
         if a != b:
             return False
         continue
-
-print(is_poliandrome(a))
+print(is_polindrome(a))
