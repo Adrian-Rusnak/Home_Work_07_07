f4d98395a75a21367ad0a5c7bb1efdfd2b2b8904
05050 <0505060173a@gmail.com>
2022-08-14 13:40:57 +0300

    Выполненo

--- DZ-8/DZ-22.8.3.py
+++ DZ-8/DZ-22.8.3.py
@@ -1,10 +1,12 @@
 def chislo(a):
+    c=[]
     b = []
     for i in a:
-        if i in b:
-            continue
+        if a .count(i)>1:
+            c.append(i)
         else:
             b.append(i)
+            return b[0]
 
-    return b
-print(chislo([1,2,1,1,1]))
+
+print(chislo([5,5,5,0.5]))
