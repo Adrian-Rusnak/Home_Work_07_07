950280de9439d890e9f2af19c8dd31baa894a1df
05050 <0505060173a@gmail.com>
2022-08-15 16:59:11 +0300

    Выполненo

--- DZ-9/DZ-23.9.1.py
+++ DZ-9/DZ-23.9.1.py
@@ -1,22 +1,7 @@
-def massiv(a):
-    if a == []:
+def massiv(*args):
+    if len(args)== 0:
         return 0
-    b= a[0]
-    c = a[0]
-    for i in a:
-        if i > b:
-            b=i
-    for i in a:
-        if i <c:
-            c=i
-    return b-c
-print(massiv([5,-5]))
-
-
-
-m =(1,2,3)
-if m==():
-    print(0)
-else:
-    c = max(m)-min(m)
-    print(c)
+    else:
+        m = max(args)-min(args)
+        return m
+print(massiv(1,2,3,5,9))
