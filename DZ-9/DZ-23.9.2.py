1cee5b12f5def7323ba0e96b721e054764e74126
05050 <0505060173a@gmail.com>
2022-08-16 16:46:30 +0300

    D

--- DZ-9/DZ-23.9.2.py
+++ DZ-9/DZ-23.9.2.py
@@ -1,8 +1,8 @@
-def popular(text:str, lst: list)-> dict:
-    dct ={}
-    text = text.lower().split()
-    for word in lst:
-        dct[word]=text.count(word)
-    return dct
-res = popular('''When I was One I had just begun When I was Two I was nearly new ''', ["i", "was","there","near"])
-print(res)
+def popular(t:str, l: list)-> dict:
+    d ={}
+    t =t.lower().split()
+    for i in l:
+        d[i]=t.count(i)
+    return d
+s = popular('''When I was One I had just begun When I was Two I was nearly new ''', ["i", "was","there","near"])
+print(s)
